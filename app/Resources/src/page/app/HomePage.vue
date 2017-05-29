ecf4b95401e8fee614f8a577319d0c98fe4c2e94
Xavi Ablaza <xlablaza@gmail.com>
2017-05-29 17:02:35 +0800

    Added latest tracks layout

--- app/Resources/src/page/app/HomePage.vue
+++ app/Resources/src/page/app/HomePage.vue
@@ -33,11 +33,9 @@
                     </div>
                 </div>
             </div>
-            <div class="container">
-                <h1 class="cr_header">What's New</h1>
-            </div>
 
             <div class="container">
+                <h1 class="cr_header">What's New</h1>
                 <div class="whatsnew">
                     <div class="row">
                         <div class="col-md-6 wn-widebox">
@@ -67,8 +65,60 @@
 
             <div class="container">
                 <h1 class="cr_header">Latest Tracks</h1>
-
+                <div class="latesttracks">
+                    <div class="row trackview">
+                        <div class="col-md-2 lt-trackbox">
+                            <img src="./../../../img/attention.jpg">
+                            <div class="lt-text">
+                                <p class="lt-title">Attention</p>
+                                <p class="lt-artist">Charlie Puth</p>
+                                <p class="lt-minsago">15m ago</p>
+                            </div>
+                        </div>
+                        <div class="col-md-2 lt-trackbox">
+                            <img src="./../../../img/adele.jpg">
+                            <div class="lt-text">
+                                <p class="lt-title">Hello</p>
+                                <p class="lt-artist">Adele</p>
+                                <p class="lt-minsago">20m ago</p>
+                            </div>
+                        </div>
+                        <div class="col-md-2 lt-trackbox">
+                            <img src="./../../../img/heatstroke.jpg">
+                            <div class="lt-text">
+                                <p class="lt-title">Heatstroke</p>
+                                <p class="lt-artist">Calvin Harris</p>
+                                <p class="lt-minsago">35m ago</p>
+                            </div>
+                        </div>
+                        <div class="col-md-2 lt-trackbox">
+                            <img src="./../../../img/morelife.jpg">
+                            <div class="lt-text">
+                                <p class="lt-title">Passionfruit</p>
+                                <p class="lt-artist">Drake</p>
+                                <p class="lt-minsago">40m ago</p>
+                            </div>
+                        </div>
+                        <div class="col-md-2 lt-trackbox">
+                            <img src="./../../../img/losemycool.jpg">
+                            <div class="lt-text">
+                                <p class="lt-title">Lose My Cool</p>
+                                <p class="lt-artist">Amber Mark</p>
+                                <p class="lt-minsago">45m ago</p>
+                            </div>
+                        </div>
+                        <div class="col-md-2 lt-trackbox">
+                            <img src="./../../../img/trackcd.svg">
+                            <div class="lt-text">
+                                <p class="lt-title">Stitches</p>
+                                <p class="lt-artist">Shawn Mendes</p>
+                                <p class="lt-minsago">50m ago</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
             <div class="container">
                 <h1 class="cr_header">Show of the Week</h1>
 
