c4abde3bb8f7cce719395c5fdac91016b04a4db4
Xavi Ablaza <xlablaza@gmail.com>
2017-06-01 11:57:24 +0800

    Added trackbox & showbox components

---
+++ app/Resources/src/components/ShowBox.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="showoftheweek">
+        <div class="row row-eq-height marginfix">
+            <div class="col-md-7 nopadding">
+                <img src="./../../img/showoftheweek.jpg">
+            </div>
+            <div class="col-md-5 sotw-text nopadding">
+                <p class="sotw-showname">{{ show_name }}</p>
+                <p class="sotw-djnames">{{ dj_names }}</p>
+                <p class="sotw-desc">{{ show_desc }}</p>
+                <a class="btn btn-full btncenter" href="#">VIEW EPISODES</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default{
+    data () {
+      return {}
+    },
+    props: {
+        show_name: {
+            type: String,
+            default: ''
+        },
+        dj_names: {
+            type: String,
+            default: ''
+        },
+        show_desc: {
+            type: String,
+            default: ''
+        }
+    },
+    methods: {
+    }
+  }
+</script>
