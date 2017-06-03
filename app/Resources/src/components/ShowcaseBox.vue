ab4be86f687518c9c066af0bf11c973562a17fdd
Xavi Ablaza <xlablaza@gmail.com>
2017-06-03 22:53:19 +0800

    Added show box for /shows

---
+++ app/Resources/src/components/ShowcaseBox.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="col-md-3 wnb">
+        <a class="scb-box" :href="showpage_url">
+            <img :src="image_url">
+            <p class="scb-text scb-text-title"><strong>{{ show_name }}</strong></p>
+            <p class="scb-text"><i>{{ genre }}</i><br><br>{{ show_description }}<br><br></p>
+            <!--<p class="scb-genre"><i>{{ genre }}</i></p>-->
+            <!--<p class="scb-desc">{{ show_description }}</p>-->
+        </a>
+    </div>
+</template>
+
+<script>
+  export default{
+    data () {
+      return {}
+    },
+    props: {
+        show_name: {
+            type: String,
+            default: ''
+        },
+        genre: {
+            type: String,
+            default: ''
+        },
+        show_description: {
+            type: String,
+            default: ''
+        },
+        showpage_url: {
+            type: String,
+            default: '#'
+        },
+        image_url: {
+            type: String,
+            default: ''
+        }
+    },
+    methods: {
+    }
+  }
+</script>
