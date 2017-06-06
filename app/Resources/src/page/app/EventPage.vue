8da0a4da7df3f20eece6d863796a0a84fa496f85
Xavi Ablaza <xlablaza@gmail.com>
2017-06-06 12:37:26 +0800

    Minor fixes to flex columns

--- app/Resources/src/page/app/EventPage.vue
+++ app/Resources/src/page/app/EventPage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <h2 class="cr_header">Events</h2>
-        <div class="row">
+        <div class="row-resp">
             <wide-box title="Greg James' Muscochella Highlights" description="Watch Greg's favorite performances from Chapman Radio's Muscochella at Musco Lawn." image_url="/bundles/public/img/dj-wide.jpeg"></wide-box>
             <small-box title="The definitive A-Z of ChapRadio's Muscochella 2017" description="26 important things that happened last weekend at Muscochella." image_url="/bundles/public/img/dj-wide.jpeg"></small-box>
             <small-box title="Muscochella Hype Playlist" description="Here's what went down when our favorite indie artists took to the stage." image_url="/bundles/public/img/dj-wide.jpeg"></small-box>
