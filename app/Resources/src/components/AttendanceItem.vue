247b07db91a9f962c4b0dbca6fb5791ce949c492
Xavi Ablaza <xlablaza@gmail.com>
2017-07-12 15:49:02 +0800

    Working on attendance page

---
+++ app/Resources/src/components/AttendanceItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <img :src="dj_image">
+        <div>
+            {{student_name}}
+            <br>
+            DJ#{{dj_number}}
+            #{{student_id}}
+        </div>
+    </div>
+</template>
+
+<script>
+  export default{
+    data () {
+      return {}
+    },
+    props: {
+      dj_image: {
+        type: String,
+        default: ''
+      },
+      student_name: {
+        type: String,
+        default: ''
+      },
+      dj_number: {
+        type: String,
+        default: ''
+      },
+      student_id: {
+        type: String,
+        default: ''
+      }
+    },
+    methods: {
+    }
+  }
+</script>
--- app/Resources/src/page/dashboard/AttendanceDashboard.vue
+++ app/Resources/src/page/dashboard/AttendanceDashboard.vue
@@ -5,11 +5,78 @@
                 <h2 class="page-header">Attendance</h2>
             </div>
         </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Recording Utility
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-md-4">
+                                Record for:
+                                <input type="radio" name="grouptype" value="everyone"> Everyone<br>
+                                <input type="radio" name="grouptype" value="class"> Class<br>
+                                <input type="radio" name="grouptype" value="newdjsandclass"> New DJs & Class<br>
+                            </div>
+                            <div class="col-md-4">
+                                Workshop Date:
+                                <datepicker></datepicker>
+                                Recording Method:
+                                <input type="radio" name="recording1" value="manual"> Manual Recording<br>
+                                <input type="radio" name="recording2" value="cardswiper"> CardSwiper<br>
+                            </div>
+                            <div class="col-md-4">
+                                <input class="form-control" placeholder="Type student ID, name, or DJ number">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <button type="button" class="btn btn-primary btn-lg btn-block">MARK PRESENT</button>
+                                <button type="button" class="btn btn-primary btn-lg btn-block">MARK EXCUSED</button>
+                                <button type="button" class="btn btn-primary btn-lg btn-block">MARK ABSENT</button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <attendance-item></attendance-item>
+                            </div>
+                            <div class="col-md-4">
+                                <attendance-item></attendance-item>
+                            </div>
+                            <div class="col-md-4">
+                                <attendance-item></attendance-item>
+                            </div>
+                            <div class="col-md-4">
+                                <attendance-item></attendance-item>
+                            </div>
+                            <div class="col-md-4">
+                                <attendance-item></attendance-item>
+                            </div>
+                            <div class="col-md-4">
+                                <attendance-item></attendance-item>
+                            </div>
+                            <div class="col-md-4">
+                                <attendance-item></attendance-item>
+                            </div>
+                            <div class="col-md-4">
+                                <attendance-item></attendance-item>
+                            </div>
+                            <div class="col-md-4">
+                                <attendance-item></attendance-item>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     /* @flow */
+    import Datepicker from 'vuejs-datepicker'
+    import AttendanceItem from '../../components/AttendanceItem.vue'
     export default{
       data () {
         return {
@@ -21,6 +88,8 @@
       watch: {
       },
       components: {
+        Datepicker,
+        AttendanceItem
       }
     }
 </script>
